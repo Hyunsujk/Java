cc1b13eb0c979bf73dd1cd0e5bf7618b7e58aafd
hyunsujk <hyunsujk@gmail.com>
2020-06-25 00:30:07 -0500

    first function practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String name = "Hyunsu";
+        sayHello(name);
     }
+
+    public static void sayHello(String s){
+        System.out.println("Hello " + s +"!");
+    }
+
 }
