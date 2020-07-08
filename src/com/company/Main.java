1340a51f247ef84e47297ad8716d9af369fa7a5c
Hyunsujk <hyunsujk@gmail.com>
2020-07-07 23:23:54 -0500

    created a short introduction log

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,33 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        String name = "Hyunsu";
-        sayHello(name);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+
+        System.out.println("How old are you? (in number)");
+        int age = Integer.parseInt(sc.nextLine());
+
+        if(age<13){
+            System.out.println("Sorry, you should be 13 years or older to use this program");
+            System.exit(0);
+        }
+
+        System.out.println("What city are you living in?");
+        String city = sc.nextLine();
+
+        System.out.println("What are you interested in?");
+        String interest = sc.nextLine();
+
+        System.out.println("Here is your summary \n-----------------\n");
+        System.out.printf("My name is %s \n", name);
+        System.out.printf("I am %s years old currently living in %s \n", age, city);
+        System.out.println("I am interested in " + interest +". Nice to meet you!");
     }
 
-    public static void sayHello(String s){
-        System.out.println("Hello " + s +"!");
-    }
 
 }
